671364e6d47a885f133f0c380d55855546cdeaec
Gabriel Silva <gabbrielssilva.biel@gmail.com>
2025-06-19 10:04:47 -0400

    resolução exercício 2

--- Algoritmos2/Funções e Procedimentos/Ex2.c
+++ Algoritmos2/Funções e Procedimentos/Ex2.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
-
+void imprime_divisores(int num1, int num2, int num3);
 
 int main(){
-
+    imprime_divisores(4,6,2);
     return 0;
 }
 
+void imprime_divisores(int num1, int num2, int num3){
+    if(num3 != 0){
+        if(num1 >= num2){
+            for(int i = num2; i <= num1; i++){
+                if(i % num3 == 0){
+                    printf("%d \n", i);
+                }
+            }
+        } else{
+            for(int i = num2; i >= num1; i--){
+                if(i % num3 == 0){
+                    printf("%d \n", i);
+                }
+            }
+        }
+    }
+}
