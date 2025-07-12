a505cbb283f5a30b36f74b843a659073072258eb
Gabriel Silva <gabbrielssilva.biel@gmail.com>
2025-07-11 22:03:16 -0400

    exercise

---
+++ Algoritmos2/Recursion/Exercise9.py
@@ -0,0 +1,11 @@
+def HexadecimalBase(decimal: int) -> str:
+    if(decimal == 0):
+        return ""
+    else:
+        if(decimal % 16 >= 10):
+            return HexadecimalBase(decimal // 16) + chr(decimal % 16 - 10 + ord('A'))
+        else:
+            return HexadecimalBase(decimal // 16) + chr(decimal % 16 + ord('0'))
+
+NumTest = 145
+print(HexadecimalBase(NumTest))
