9778b246100f7ee096c9f8645a02be60e3585ab7
Gabriel Silva <149592906+Gabrielzxd@users.noreply.github.com>
2025-07-11 20:01:44 -0400

    Delete Algoritmos2/Recursão directory

--- Algoritmos2/Recursão/Ex3.py
+++
@@ -1,8 +0,0 @@
-def inverterNumero(num):
-    def A(num, resultado):
-        if(num == 0):
-            return resultado
-        else:
-            return A(num // 10, resultado * 10 + num % 10)
-        
-    return inverterNumero(num, 0)
