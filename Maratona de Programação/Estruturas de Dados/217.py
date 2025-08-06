4e8fbad0ac6f30e7b7110f4308d077d28589ebfa
Gabriel Silva <gabbrielssilva.biel@gmail.com>
2025-08-06 15:31:55 -0400

    atualização

---
+++ Maratona de Programação/Estruturas de Dados/217.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        setnums = set(nums)
+        num = 3.14157
+        num = round(num, ndigits=2)
+        print(num)
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
---
+++ Python_Aleatoriedades/linkedlist.py
@@ -0,0 +1,12 @@
+from node import Nodeleme
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+    
+    def append(self, elem):
+        if self.head:
+            pass
+        else:
+            Node(elem)
